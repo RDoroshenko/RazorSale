31e45e0b0288a95e7f8d283f30149d8d0638cd5c
Roman Doroshenko <r.m.doroshenko@gmail.com>
2016-12-26 17:09:22 +0300

    Added order options: - Razor - Razor and Gel - Razor, Gel and Aftershave

--- tests/RazorShopTests.js
+++ tests/RazorShopTests.js
@@ -9,4 +9,18 @@ suite ('When I order razor set', function () {
 
         assert.equal(razorOrdered, true);
     })
+    test('When I order razor and gel set then razor and gel set is added to order', function () {
+        let order = new Order;
+
+        let razorAndGelOrdered = order.add('razorAndGelSet');
+
+        assert.equal(razorAndGelOrdered, true);
+    })
+    test('When I order razor, gel and aftershave set then razor, gel and aftershave set is added to order', function () {
+        let order = new Order;
+
+        let razorGelAftershaveOrdered = order.add('razorGelAftershaveSet');
+
+        assert.equal(razorGelAftershaveOrdered, true);
+    })
 })
